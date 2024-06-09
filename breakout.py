00c35b324f98168314bc1be548caeca430704257
Tegan Bold <tegan.bold@gmail.com>
2024-06-08 23:31:12 -0400

    Add moving ball

--- breakout.py
+++ breakout.py
@@ -23,8 +23,6 @@ blue = (90, 220, 237)
 cols = 6
 rows = 6
 
-screen.fill(bg_color)
-
 class Wall():
     def __init__(self) -> None:
         self.width = screen_width // cols
@@ -63,16 +61,83 @@ class Wall():
                 pygame.draw.rect(screen, bg_color, block[0], 2)
 
 
+class Paddle():
+    def __init__(self) -> None:
+        self.width = screen_width // cols
+        self.height = 20
+        self.x = (screen_width // 2) - (self.width // 2)
+        self.y = screen_height - (self.height * 2)
+        self.speed = 10
+        self.rect = pygame.Rect(self.x, self.y, self.width, self.height)
+        self.color = paddle_color
+        self.direction = 0
+
+    def draw_paddle(self):
+        pygame.draw.rect(screen, paddle_color, self.rect)
+
+    def move(self):
+        self.direction = 0
+        key = pygame.key.get_pressed()
+        if key[K_LEFT] and self.rect.left > 0:
+            self.rect.x -= self.speed
+            self.direction = -1
+        if key[K_RIGHT] and self.rect.right < screen_width:
+            self.rect.x += self.speed
+            self.direction = 1
+
+class Ball():
+    def __init__(self) -> None:
+        self.width = 10
+        self.height = 10
+        self.x = (screen_width // 2) - (self.width // 2)
+        self.y = screen_height - (self.height * 2) - 30
+        self.rect = pygame.Rect(self.x, self.y, self.width, self.height)
+        self.speed = 5
+        self.direction = [1, 1]
+        self.ball_moving = False
+
+    def move(self):
+        if self.ball_moving:
+            self.rect.x += self.speed * self.direction[0]
+            self.rect.y += self.speed * -self.direction[1]
+        if self.rect.right == screen_width:
+            self.direction[0] = -self.direction[0]
+        if self.rect.top == 0:
+            self.direction[1] = -self.direction[1]
+        if self.rect.left == 0:
+            self.direction[0] = -self.direction[0]
+        
+
+        # Start the game
+        key = pygame.key.get_pressed()
+        if key[K_SPACE]:
+            self.ball_moving = True
+
+    def reverse_direction(self):
+        pass
+
+
+    def draw(self):
+        pygame.draw.circle(screen, ball_color, (self.rect.x, self.rect.y), self.width)
+
 
 wall = Wall()
+paddle = Paddle()
+ball = Ball()
 wall.build_wall()
 
 run = True
 while run:
     clock.tick(60)
 
+    screen.fill(bg_color)
     wall.draw_wall()
+    ball.draw()
+    ball.move()
+    paddle.draw_paddle()
+    paddle.move()
 
+    
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
